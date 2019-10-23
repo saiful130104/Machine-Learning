528a00661d21215599e5b28eafa42bfe2da9ad7d
Saiful Islam <s.islam@bjitgroup.com>
2019-10-23 16:43:20 +0600

    Application of some functions like find max,min etc in NumPy

---
+++ NumPy_Practice/universal functions in numpy.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+# create an array of sine values
+ara = np.array([0, np.pi / 2, np.pi])
+print("Sine values of array elements:", np.sin(ara))
+
+# exponential values
+ara = np.array([0, 1, 2, 3])
+print("Exponent of array elements:", np.exp(ara))
+
+# square root of array values
+print("Square root of array elements:", np.sqrt(ara))
