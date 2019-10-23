3ab0001002cd68c01900128bc15b8e8fb9c84298
Saiful Islam <s.islam@bjitgroup.com>
2019-10-23 08:22:24 +0600

    NumPy basics learning and practice files

---
+++ NumPy_Practice/basic array characteristics.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+ara = np.array([[1, 2, 3.5],
+                [3, 4, 5]])
+
+print("Array type is : ",type(ara))
+print("Array dimensions is : ",ara.ndim)
+print("Array shape is : ",ara.shape)
+print("Array size is : ",ara.size)
+print("Array elements type is : ",ara.dtype)
